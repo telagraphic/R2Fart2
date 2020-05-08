fc9b2740da403947c08266b63e32f3fbbb49d7a4
telagraphic <telagraphic@gmail.com>
2020-05-08 12:37:49 -0400

    🛠️ FIX: added html5 audio option for iOS Safari sounds

--- js/sounds.js
+++ js/sounds.js
@@ -9,61 +9,71 @@ export const soundEffects = [
   {
     sound: new Howl({
       src: ['./audio/sfx-2.mp3','./audio/sfx-2.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-3.mp3','./audio/sfx-3.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-4.mp3','./audio/sfx-4.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-5.mp3','./audio/sfx-5.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-6.mp3','./audio/sfx-6.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-7.mp3','./audio/sfx-7.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-8.mp3', './audio/sfx-8.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-9.mp3', './audio/sfx-9.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-10.mp3', './audio/sfx-10.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   },
   {
     sound: new Howl({
       src: ['./audio/sfx-11.mp3', './audio/sfx-11.wav'],
-      volume: 1
+      volume: 1,
+      html5: true
     })
   }
 ]
