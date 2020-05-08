9a03ccdb9724fdcde5b618cdbfe5e67381427b20
telagraphic <telagraphic@gmail.com>
2020-05-08 12:27:52 -0400

    🗄️ PROJECT STRUCTURE: removed hammer.min.js and used CDN and created sounds.js for audio files

--- js/app.js
+++ js/app.js
@@ -3,92 +3,11 @@ import {
   fartTwo,
   fartThree
 } from './animations.js';
+
+import {soundEffects} from './sounds.js';
+
 const button = document.querySelector('.artwork');
 const mobileButton = new Hammer(button);
-
-const soundEffects = [
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-1.mp3','./audio/sfx-1.wav'],
-      volume: 1,
-      onend: function() {
-        console.log('1');
-      }
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-2.mp3','./audio/sfx-2.wav'],
-      volume: 1,
-      onend: function() {
-        console.log('2');
-      }
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-3.mp3','./audio/sfx-3.wav'],
-      volume: 1,
-      onend: function() {
-        console.log('3');
-      }
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-4.mp3','./audio/sfx-4.wav'],
-      volume: 1,
-      onend: function() {
-        console.log('4');
-      }
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-5.mp3','./audio/sfx-5.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-6.mp3','./audio/sfx-6.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-7.mp3','./audio/sfx-7.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-8.mp3', './audio/sfx-8.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-9.mp3', './audio/sfx-9.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-10.mp3', './audio/sfx-10.wav'],
-      volume: 1
-    })
-  },
-  {
-    sound: new Howl({
-      src: ['./audio/sfx-11.mp3', './audio/sfx-11.wav'],
-      volume: 1,
-      onend: function() {
-        console.log('11');
-      }
-    })
-  }
-]
 
 let index = 0;
 
---
+++ js/sounds.js
@@ -0,0 +1,68 @@
+export const soundEffects = [
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-1.mp3','./audio/sfx-1.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-2.mp3','./audio/sfx-2.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-3.mp3','./audio/sfx-3.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-4.mp3','./audio/sfx-4.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-5.mp3','./audio/sfx-5.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-6.mp3','./audio/sfx-6.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-7.mp3','./audio/sfx-7.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-8.mp3', './audio/sfx-8.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-9.mp3', './audio/sfx-9.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-10.mp3', './audio/sfx-10.wav'],
+      volume: 1
+    })
+  },
+  {
+    sound: new Howl({
+      src: ['./audio/sfx-11.mp3', './audio/sfx-11.wav'],
+      volume: 1
+    })
+  }
+]
